f2bc54d7179d10d6ef1c8981a310b48401614c85
Okalashnykov <Okalashnykov@allvuesystems.com>
2024-03-28 16:09:44 +0200

    HW1 (Commit)

--- src/ДЗ. TypeScript. Анотація типів та примітивні типи.ts
+++ src/ДЗ. TypeScript. Анотація типів та примітивні типи.ts
@@ -1,67 +1,72 @@
 class School {
-  directions = [];
+  directions: string[] = [];
 
-  addDirection(direction) {
+  addDirection(direction: string): void {
     this.directions.push(direction);
   }
 }
 
 class Direction {
-  levels = [];
+  levels: string[] = [];
+  _name: string;
 
-  get name() {
+  get name(): string {
     return this._name;
   }
 
-  constructor(name) {
+  constructor(name: string) {
     this._name = name;
   }
 
-  addLevel(level) {
+  addLevel(level: string): void {
     this.levels.push(level);
   }
 }
 
 class Level {
-  groups = [];
+  groups: string[] = [];
+  _name: string;
+  _program: number;
 
-  constructor(name, program) {
-    this.name = name;
-    this._program = program;
-  }
-
-  get name() {
+  get name(): string {
     return this._name;
   }
 
-  get program() {
+  get program(): number {
     return this._program;
   }
 
-  addGroup(group) {
+  constructor(name: string, program: number) {
+    this._name = name;
+    this._program = program;
+  }
+
+  addGroup(group: string): void {
     this.groups.push(group);
   }
 }
 
 class Group {
-  _students = [];
+  _students: string[] = [];
+  directionName: string;
+  levelName: string;
 
-  get students() {
+  get students(): string[] {
     return this._students;
   }
 
-  constructor(directionName, levelName) {
+  constructor(directionName: string, levelName: string) {
     this.directionName = directionName;
     this.levelName = levelName;
   }
 
-  addStudent(student) {
+  addStudent(student: string): void {
     this._students.push(student);
   }
 
-  showPerformance() {
+  showPerformance(): string[] {
     const sortedStudents = this.students.toSorted(
-      (a, b) => b.getPerformanceRating() - a.getPerformanceRating()
+      (a: any, b: any) => b.getPerformanceRating() - a.getPerformanceRating()
     );
 
     return sortedStudents;
@@ -69,47 +74,48 @@ class Group {
 }
 
 class Student {
-  grades = {};
-  attendance = [];
+  grades: any = {};
+  attendance: boolean[] = [];
+  firstName: string;
+  lastName: string;
+  birthYear: number;
 
-  constructor(firstName, lastName, birthYear) {
+  get fullName(): string {
+    return `${this.lastName} ${this.firstName}`;
+  }
+
+  set fullName(value: string) {
+    [this.lastName, this.firstName] = value.split(' ');
+  }
+
+  get age(): number {
+    return new Date().getFullYear() - this.birthYear;
+  }
+
+  constructor(firstName: string, lastName: string, birthYear: number) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.birthYear = birthYear;
   }
 
-  get fullName() {
-    return `${this.lastName} ${this.firstName}`;
-  }
-
-  set fullName(value) {
-    [this.lastName, this.firstName] = value.split(" ");
-  }
-
-  get age() {
-    return new Date().getFullYear() - this.birthYear;
-  }
-
-  setGrade(subject, grade) {
+  setGrade(subject: string | number, grade: number): void {
     this.grades[subject] = grade;
   }
 
-  markAttendance(present) {
+  markAttendance(present: boolean): void {
     this.attendance.push(present);
   }
 
-  getPerformanceRating() {
-    const gradeValues = Object.values(this.grades);
+  getPerformanceRating(): number {
+    const gradeValues: number[] = Object.values(this.grades);
 
     if (gradeValues.length === 0) return 0;
 
-    const averageGrade =
-      gradeValues.reduce((sum, grade) => sum + grade, 0) / gradeValues.length;
+    const averageGrade: number =
+      gradeValues.reduce((sum: number, grade: number) => sum + grade, 0) / gradeValues.length;
 
-    const attendancePercentage =
-      (this.attendance.filter((present) => present).length /
-        this.attendance.length) *
-      100;
+    const attendancePercentage: number =
+      (this.attendance.filter((present: boolean): boolean => present).length / this.attendance.length) * 100;
 
     return (averageGrade + attendancePercentage) / 2;
   }
